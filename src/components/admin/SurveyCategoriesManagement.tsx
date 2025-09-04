1c094060feba74204122f8d46f208b08fd98cf26
Ramita De <romi.de2010@gmail.com>
2025-09-04 13:48:09 +0530

    fixes

--- src/components/admin/SurveyCategoriesManagement.tsx
+++ src/components/admin/SurveyCategoriesManagement.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Plus, Pencil, Trash2, Search } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
